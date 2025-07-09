c2c3f8e5d6347ae631f5859250444f056b9d24b7
Genki Otake <231205037@ccmailg.meijo-u.ac.jp>
2025-07-09 10:06:39 +0900

    メール確認機能の改修

---
+++ js/login_confirm.js
@@ -0,0 +1,40 @@
+import { app } from "./firebase_init.js";
+import {
+    getAuth,
+    onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+import {
+    getFirestore,
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+
+const auth = getAuth(app);
+
+$(async function waitForEmailVerification() {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    onAuthStateChanged(auth, async function (user) {
+        const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+        let attempts = 0;
+        const maxAttempts = 100;
+
+        while (attempts < maxAttempts) {
+            await user.reload(); //最新状態にする
+
+            if (user.emailVerified) {
+                console.log("メール確認済み！");
+                window.location.href = "index.html";
+                return;
+            }
+
+            console.log(`確認チェック ${attempts + 1} 回目...まだ未確認`);
+            await delay(5000);
+            attempts++;
+        }
+
+        //タイムアウト
+        alert("一定時間内にメールが確認されませんでした。");
+        window.location.href = "login.html";
+    })
+})
