f9c4cafc1bbba4acf2787a5360b3d5b7d3d83f1f
Genki Otake <231205037@ccmailg.meijo-u.ac.jp>
2025-07-10 11:03:08 +0900

    確認メールの再送に1分の待ち時間を作るようにした

--- js/login_confirm.js
+++ js/login_confirm.js
@@ -4,16 +4,15 @@ import {
     onAuthStateChanged,
     sendEmailVerification,
 } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
-import {
-    getFirestore,
-} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
 
+
+//確認メールの処理
 const auth = getAuth(app);
-
 $(async function waitForEmailVerification() {
     const auth = getAuth();
     const user = auth.currentUser;
 
+    //ユーザーの状態が変化したら発火
     onAuthStateChanged(auth, async function (user) {
         const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
@@ -40,9 +39,39 @@ $(async function waitForEmailVerification() {
     })
 })
 
+//初期表示
+$(toggle_active_mail_resend_btn());
+
+//再送メールを送る処理
 $('#resend').click(function(){
     const user = auth.currentUser;
     sendEmailVerification(user).then(() => {
         alert("メールを再送しました。"); /* 成功メッセージ*/
+        toggle_active_mail_resend_btn();
     });
+    
 })
+
+//メール再送ボタンを5秒間非アクティブにする関数
+async function toggle_active_mail_resend_btn(){
+    const resend_btn = $('button[id="resend"]');
+    resend_btn.prop('disabled', true); 
+    //元のテキストと背景色を保持
+    const btn_text = resend_btn.text();
+    const btn_color = resend_btn.css('background-color');
+
+    let attempts = 0;
+    const maxAttempts = 60;
+    //5秒カウント
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+    while (attempts < maxAttempts) {
+        resend_btn.text(`${60-attempts}後に再送可能`);
+        resend_btn.css('background-color', 'grey');
+        await delay(1000);
+        attempts++;
+    }
+    //元に戻す
+    resend_btn.text(btn_text);
+    resend_btn.css('background-color', btn_color);
+    resend_btn.prop('disabled', false); 
+}
